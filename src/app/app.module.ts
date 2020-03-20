f873206570abd881182049c2913feac276341619
Ghita BOUAYAD <bouayad.ghita0@gmail.com>
2020-03-20 09:19:24 +0100

    Add the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { CoreModule } from './core/core.module';
 import{ReposModule} from './repos/repos.module'
 import {SharedModule} from './shared/shared.module'
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
